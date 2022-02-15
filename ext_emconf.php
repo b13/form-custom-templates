d498cd19dfb5c0b473c11d82e208c0d381044ed5
ochorocho <_elOcho84_>
2022-02-15 21:50:52 +0100

    first commit

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'B13 Form Email Template',
+    'description' => 'Selectable Email Template in Finisher',
+    'category' => 'be',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 0,
+    'author' => 'TEST',
+    'author_email' => 'info@b13.com',
+    'author_company' => '',
+    'version' => '11.5.4',
+    'constraints' => [
+        'depends' => [],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
