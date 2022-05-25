567ba6d9f14e519eeaa1624e175f8d1e6859a16a
ochorocho <jochen.roth@b13.com>
2022-05-25 14:55:47 +0200

    [BUGFIX] Do not overwrite default palettes
    * Removed palettes override
    * Fixed icon identifier for custom ctype

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -17,7 +17,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
         [
             'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/Database:form_custom_templates.pageType',
             $emailDoktype,
-            'page-email',
+            'apps-pagetree-page-email',
             'special'
         ],
         '254',
@@ -34,22 +34,10 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
                     $emailDoktype . '-hideinmenu' => 'apps-pagetree-page-email-hideinmenu',
                 ],
             ],
-            // add all page standard fields and tabs to your new page type
+            // Show only fields useful in email context
             'types' => [
                 $emailDoktype => [
-                    //'showitem' => $GLOBALS['TCA'][$table]['types'][PageRepository::DOKTYPE_DEFAULT]['showitem']
                     'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general, --palette--;;standard, --palette--;;title, --div--;LLL:EXT:seo/Resources/Private/Language/locallang_tca.xlf:pages.tabs.seo, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.appearance, --palette--;;layout, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.behaviour, --palette--;;miscellaneous, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.resources, --palette--;;media, --palette--;;config, --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language, --palette--;;language, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.access, --palette--;;visibility, --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended',
-                ]
-            ],
-            'palettes' => [
-                'layout' => [
-                    'showitem' => 'layout, --linebreak--,backend_layout, backend_layout_next_level'
-                ],
-                'miscellaneous' => [
-                    'showitem' => 'is_siteroot;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.is_siteroot_formlabel, no_search;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.no_search_formlabel'
-                ],
-                'visibility' => [
-                    'showitem' => 'hidden;LLL:EXT:core/Resources/Private/Language/locallang_tca.xlf:pages.hidden_toggle_formlabel'
                 ]
             ]
         ]
