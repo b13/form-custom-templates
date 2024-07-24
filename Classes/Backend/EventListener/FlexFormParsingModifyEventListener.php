5513565b2a68884d0a86b72f1a37639ef37837b5
Daniel Goerz <daniel.goerz@b13.com>
2024-07-24 16:01:12 +0200

    [BUGFIX] Also check for new config path without TCEforms

--- Classes/Backend/EventListener/FlexFormParsingModifyEventListener.php
+++ Classes/Backend/EventListener/FlexFormParsingModifyEventListener.php
@@ -15,25 +15,28 @@ final class FlexFormParsingModifyEventListener
     {
         $identifier = $event->getIdentifier();
 
-        if (($identifier['ext-form-overrideFinishers'] ?? '') === 'enabled') {
-            $addToFinishers = ['EmailToSender', 'EmailToReceiver'];
-            $options = $this->emailTemplateService->getOptions();
-            $dataStructure = $event->getDataStructure();
+        if (($identifier['ext-form-overrideFinishers'] ?? '') !== 'enabled') {
+            return;
+        }
 
-            // Search for finishers and add items
-            foreach ($dataStructure['sheets'] as $sheetIdentifier => $sheet) {
-                foreach ($addToFinishers as $finisherIdentifier) {
-                    if ($dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid'] ?? false) {
-                        $allOptions = array_merge(
-                            $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid']['TCEforms']['config']['items'],
-                            $options
-                        );
-                        $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid']['TCEforms']['config']['items'] = $allOptions;
-                    }
+        $addToFinishers = ['EmailToSender', 'EmailToReceiver'];
+        $options = $this->emailTemplateService->getOptions();
+        $dataStructure = $event->getDataStructure();
+
+        // Search for finishers and add items
+        foreach ($dataStructure['sheets'] as $sheetIdentifier => $sheet) {
+            foreach ($addToFinishers as $finisherIdentifier) {
+                if ($dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid'] ?? false) {
+                    $allOptions = array_merge(
+                        $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid']['TCEforms']['config']['items'] ??
+                        $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid']['config']['items'] ?? [],
+                        $options
+                    );
+                    $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid']['TCEforms']['config']['items'] = $allOptions;
                 }
             }
-
-            $event->setDataStructure($dataStructure);
         }
+
+        $event->setDataStructure($dataStructure);
     }
 }
