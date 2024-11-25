b4762f8fb636f3a64097c66aa5f3136feee2c985
Daniel Goerz <daniel.goerz@b13.com>
2024-11-25 15:44:10 +0100

    [BUGFIX] Make template pages available in form overrides

--- Classes/Backend/EventListener/FlexFormParsingModifyEventListener.php
+++ Classes/Backend/EventListener/FlexFormParsingModifyEventListener.php
@@ -26,13 +26,45 @@ final class FlexFormParsingModifyEventListener
         // Search for finishers and add items
         foreach ($dataStructure['sheets'] as $sheetIdentifier => $sheet) {
             foreach ($addToFinishers as $finisherIdentifier) {
-                if ($dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid'] ?? false) {
-                    $allOptions = array_merge(
-                        $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid']['TCEforms']['config']['items'] ??
-                        $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid']['config']['items'] ?? [],
-                        $options
-                    );
-                    $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el']['settings.finishers.' . $finisherIdentifier . '.emailTemplateUid']['TCEforms']['config']['items'] = $allOptions;
+                $fieldName = 'settings.finishers.' . $finisherIdentifier . '.emailTemplateUid';
+
+                // Field name not found? Continue.
+                if (!($dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName] ?? false)) {
+                    continue;
+                }
+
+                // @deprecated - this code block can be removed when support for TYPO3 v11 is dropped
+                if (is_array($dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['TCEforms']['config']['items'] ?? null)) {
+                    $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['TCEforms']['config']['items'] =
+                        array_merge(
+                            $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['TCEforms']['config']['items'],
+                            $options
+                        );
+                    continue;
+                }
+
+                // V12 - Add options to select and fix default value
+                if (is_array($dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['config']['items'] ?? null)) {
+                    if ((int)($dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['config']['default'] ?? 0) > 0) {
+                        // Replacing the Page ID with the page Title in the field label
+                        foreach ($options as $option) {
+                            if ((int)($option['value'] ?? -1) !== (int)($dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['config']['default'] ?? 0)) {
+                                continue;
+                            }
+                            if (empty($option['label'])) {
+                                continue;
+                            }
+                            $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['label'] =
+                                str_replace(
+                                    (string)$dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['config']['default'],
+                                    $option['label'],
+                                    $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['label']
+                                );
+                            break;
+                        }
+                    }
+
+                    $dataStructure['sheets'][$sheetIdentifier]['ROOT']['el'][$fieldName]['config']['items'] = $options;
                 }
             }
         }
